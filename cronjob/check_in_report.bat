1e287b4c61b5be2b1f2a446c5c4f9624987424d0
beckpalmx77 <beckpalmx@gmail.com>
2024-09-14 17:43:37 +0700

    up

--- cronjob/check_in_report.bat
+++ cronjob/check_in_report.bat
@@ -1,5 +1,1 @@
-php generate_job_payment.php
-php calculate_job_payment.php
-php import_data_deptpartment.php
-php import_data_employee.php
-php import_data_employee_update.php
+php check_in_report.php
